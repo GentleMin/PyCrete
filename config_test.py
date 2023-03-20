eaad9d089481876f34740b56be72ef3deca74ef4
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-03-20 23:39:45 +0100

    Configure test cases

--- config_test.py
+++ config_test.py
@@ -8,16 +8,16 @@ def m_u(s):
     return np.ones(s.shape)
 
 def k_uu2(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def k_uu1(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def k_ub1(s):
     return np.ones(s.shape)
     
 def k_ub0(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def k_bu1(s):
     return np.ones(s.shape)
@@ -26,17 +26,17 @@ def m_b(s):
     return np.ones(s.shape)
 
 def k_bb2(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def k_bb1(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def k_bb0(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def dk_uu2(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
 def dk_bb2(s):
-    return np.zeros(s.shape)
+    return np.ones(s.shape)
 
