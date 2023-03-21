a744ebc2f6a3059e45d1a5bd887c05a55dbbd152
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-03-21 23:55:32 +0100

    Convert FEM code to complete sparse formulation

--- taw_eigen_fem.py
+++ taw_eigen_fem.py
@@ -13,7 +13,8 @@ import h5py
 """IO Setting"""
 
 save_eigen = True
-save_eigen_fname = "./output/eigenmodes_Pm0"
+save_eigen_fname = "./output/eigenmodes_Pm0_n100"
+save_pattern_fname = "./output/specspy_TO_Pm0_fem"
 
 sparse_solver = True
 
@@ -28,7 +29,7 @@ import config_TO as cfg
 """Mesh generation"""
 
 # Maximum element size
-ds_max = 0.02
+ds_max = 0.01
 # Collocation: the meshes are of the same type and overlap
 elem = mesh.QuadraticElement()
 # Quadrature rules
@@ -67,9 +68,19 @@ the global matrices remain unchanged and can be reused
 N_vals = elem.f_shape(xi_quad)
 dN_vals = elem.f_dshape(xi_quad)
 
-# Pre-allocate global matrices -----> convert to fully sparse formulation in the future
-K_mat = np.zeros((n_dof, n_dof))
-M_mat = np.zeros((n_dof, n_dof))
+# Pre-allocate global matrices ----- historical dense formulation
+# K_mat = np.zeros((n_dof, n_dof))
+# M_mat = np.zeros((n_dof, n_dof))
+
+# Sparse formulation
+stride = elem.n_nodes**2
+K_vals = np.zeros(4*n_elem*stride)
+K_rows = np.zeros(4*n_elem*stride, dtype=np.int32)
+K_cols = np.zeros(4*n_elem*stride, dtype=np.int32)
+
+M_vals = np.zeros(2*n_elem*stride)
+M_rows = np.zeros(2*n_elem*stride, dtype=np.int32)
+M_cols = np.zeros(2*n_elem*stride, dtype=np.int32)
 
 for i_elem in range(n_elem):
     
@@ -90,24 +101,48 @@ for i_elem in range(n_elem):
     Kuu1 = np.sum(wt_quad*jac_temp*(cfg.k_uu1(pt_quad) - cfg.dk_uu2(pt_quad))*Kuu1, axis=-1)
     Kuu2 = np.stack([np.outer(dN_ds[:, i], dN_ds[:, i]) for i in range(n_quad)], axis=-1)
     Kuu2 = np.sum(-wt_quad*jac_temp*cfg.k_uu2(pt_quad)*Kuu2, axis=-1)
-    K_mat[np.ix_(dof_temp_u, dof_temp_u)] += Kuu1 + Kuu2
+    
+    # K_mat[np.ix_(dof_temp_u, dof_temp_u)] += Kuu1 + Kuu2
+    iptr_slice = slice((4*i_elem)*stride, (4*i_elem + 1)*stride)
+    K_vals[iptr_slice] = (Kuu1 + Kuu2).flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_u, dof_temp_u, indexing='ij')
+    K_rows[iptr_slice] = i_rows.flatten()
+    K_cols[iptr_slice] = i_cols.flatten()
     
     # Kub submatrix
     Kub1 = np.stack([np.outer(N_vals[:, i], dN_ds[:, i]) for i in range(n_quad)], axis=-1)
     Kub1 = np.sum(wt_quad*jac_temp*cfg.k_ub1(pt_quad)*Kub1, axis=-1)
     Kub0 = np.stack([np.outer(N_vals[:, i], N_vals[:, i]) for i in range(n_quad)], axis=-1)
     Kub0 = np.sum(wt_quad*jac_temp*cfg.k_ub0(pt_quad)*Kub0, axis=-1)
-    K_mat[np.ix_(dof_temp_u, dof_temp_b)] += Kub1 + Kub0
+    
+    # K_mat[np.ix_(dof_temp_u, dof_temp_b)] += Kub1 + Kub0
+    iptr_slice = slice((4*i_elem + 1)*stride, (4*i_elem + 2)*stride)
+    K_vals[iptr_slice] = (Kub1 + Kub0).flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_u, dof_temp_b, indexing='ij')
+    K_rows[iptr_slice] = i_rows.flatten()
+    K_cols[iptr_slice] = i_cols.flatten()
     
     # Mu submatrix
     Mu = np.stack([np.outer(N_vals[:, i], N_vals[:, i]) for i in range(n_quad)], axis=-1)
     Mu = np.sum(wt_quad*jac_temp*cfg.m_u(pt_quad)*Mu, axis=-1)
-    M_mat[np.ix_(dof_temp_u, dof_temp_u)] += Mu
+    
+    # M_mat[np.ix_(dof_temp_u, dof_temp_u)] += Mu
+    iptr_slice = slice((2*i_elem)*stride, (2*i_elem + 1)*stride)
+    M_vals[iptr_slice] = Mu.flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_u, dof_temp_u, indexing='ij')
+    M_rows[iptr_slice] = i_rows.flatten()
+    M_cols[iptr_slice] = i_cols.flatten()
     
     # Kbu submatrix
     Kbu = np.stack([np.outer(N_vals[:, i], dN_ds[:, i]) for i in range(n_quad)], axis=-1)
     Kbu = np.sum(wt_quad*jac_temp*cfg.k_bu1(pt_quad)*Kbu, axis=-1)
-    K_mat[np.ix_(dof_temp_b, dof_temp_u)] += Kbu
+    
+    # K_mat[np.ix_(dof_temp_b, dof_temp_u)] += Kbu
+    iptr_slice = slice((4*i_elem + 2)*stride, (4*i_elem + 3)*stride)
+    K_vals[iptr_slice] = Kbu.flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_b, dof_temp_u, indexing='ij')
+    K_rows[iptr_slice] = i_rows.flatten()
+    K_cols[iptr_slice] = i_cols.flatten()
     
     # Kbb submatrix
     Kbb2 = np.stack([np.outer(dN_ds[:, i], dN_ds[:, i]) for i in range(n_quad)], axis=-1)
@@ -116,20 +151,29 @@ for i_elem in range(n_elem):
     Kbb1 = np.sum(wt_quad*jac_temp*(cfg.k_bb1(pt_quad) - cfg.dk_bb2(pt_quad))*Kbb1, axis=-1)
     Kbb0 = np.stack([np.outer(N_vals[:, i], N_vals[:, i]) for i in range(n_quad)], axis=-1)
     Kbb0 = np.sum(wt_quad*jac_temp*cfg.k_bb0(pt_quad)*Kbb0, axis=-1)
-    K_mat[np.ix_(dof_temp_b, dof_temp_b)] += Kbb2 + Kbb1 + Kbb0
+    
+    # K_mat[np.ix_(dof_temp_b, dof_temp_b)] += Kbb2 + Kbb1 + Kbb0
+    iptr_slice = slice((4*i_elem + 3)*stride, (4*i_elem + 4)*stride)
+    K_vals[iptr_slice] = (Kbb2 + Kbb1 + Kbb0).flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_b, dof_temp_b, indexing='ij')
+    K_rows[iptr_slice] = i_rows.flatten()
+    K_cols[iptr_slice] = i_cols.flatten()
     
     # Mb submatrix
     Mb = np.stack([np.outer(N_vals[:, i], N_vals[:, i]) for i in range(n_quad)], axis=-1)
     Mb = np.sum(wt_quad*jac_temp*cfg.m_b(pt_quad)*Mb, axis=-1)
-    M_mat[np.ix_(dof_temp_b, dof_temp_b)] += Mb
+    
+    # M_mat[np.ix_(dof_temp_b, dof_temp_b)] += Mb
+    iptr_slice = slice((2*i_elem + 1)*stride, (2*i_elem + 2)*stride)
+    M_vals[iptr_slice] = Mb.flatten()
+    i_rows, i_cols = np.meshgrid(dof_temp_b, dof_temp_b, indexing='ij')
+    M_rows[iptr_slice] = i_rows.flatten()
+    M_cols[iptr_slice] = i_cols.flatten()
 
+# Assemble sparse matrices
+M_mat = sparse.coo_array((M_vals, (M_rows, M_cols)), shape=(n_dof, n_dof)).tocsr()
+K_mat = sparse.coo_array((K_vals, (K_rows, K_cols)), shape=(n_dof, n_dof)).tocsr()
 
-# print(Kuu2 + Kuu1)
-# print(Kub1 + Kub0)
-# print(Kbu)
-# print(Kbb2 + Kbb1 + Kbb0)
-# print(Mu)
-# print(Mb)
 
 """Applying homogeneous Dirichlet boundary conditions"""
 
@@ -137,7 +181,8 @@ idx_rem = [i for i in range(n_dof) if i not in (1, n_dof - 1)]
 M_mat = M_mat[np.ix_(idx_rem, idx_rem)]
 K_mat = K_mat[np.ix_(idx_rem, idx_rem)]
 
-print(np.linalg.cond(K_mat), np.linalg.cond(M_mat))
+# print(np.linalg.cond(K_mat), np.linalg.cond(M_mat))
+
 
 """Solving the eigenvalue problem"""
 if sparse_solver:
@@ -145,10 +190,14 @@ if sparse_solver:
 else:
     w, v = linalg.eig(K_mat, M_mat)
 
+
+"""Outputs"""
+
+# Save to HDF5 file
 if save_eigen:
     with h5py.File(save_eigen_fname + ".h5", 'x') as f_write:
         f_write.create_dataset("nodes", data=gcoord_node)
-        f_write.create_dataset("cond", data=np.array([np.linalg.cond(K_mat), np.linalg.cond(M_mat)]))
+        # f_write.create_dataset("cond", data=np.array([np.linalg.cond(K_mat), np.linalg.cond(M_mat)]))
         idx_sort = np.argsort(w)
         w_sort = w[idx_sort]
         v_sort = np.zeros((n_dof, v.shape[1]), dtype=np.complex128)
@@ -156,3 +205,17 @@ if save_eigen:
         f_write.create_dataset("eigenvals", data=w_sort)
         f_write.create_dataset("eigenfuns", data=v_sort)
 
+import matplotlib.pyplot as plt
+
+# Sparse pattern plot
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+ax = axes[0]
+ax.spy(K_mat, markersize=1.)
+ax.set_title("K")
+ax = axes[1]
+ax.spy(M_mat, markersize=1.)
+ax.set_title("M")
+
+if save_pattern_fname is not None:
+    plt.savefig(save_pattern_fname + '.png', format='png', dpi=150, bbox_inches="tight")
+plt.show()
